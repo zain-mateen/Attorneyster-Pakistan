7c286f636089f1aae140e1cb149561f2738f0687
Zain Mateen <zaini_2223@yahoo.com>
2024-05-12 14:52:51 +0500

    Pricing Page added in the Website

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PracticeAreaPage from './components/PracticeAreaPage/PracticeAreaPage';
 import PracticeAreaDetail from './components/PracticeAreaSingle/PracticeAreaSingle';
 import CasesPage from './components/CasesPage/CasesPage';
 import CaseSinglePage from './components/CasesSinglePage/CasesSinglePage';
+import PricingPage from './components/PricingPage/PricingPage';
 import Footer from './components/Footer/Footer';
 
 import './App.scss';
@@ -26,7 +27,8 @@ function App() {
         <Route path='/practiceareapage' element={<PracticeAreaPage />} />
         <Route path='/practiceareasingle' element={<PracticeAreaDetail />} />
         <Route path='/cases' element={<CasesPage />} />
-        <Route path='/casesInner' element={<CaseSinglePage />} />
+        <Route path='/casesInner' element={<CaseSinglePage />} />        
+        <Route path='/packages' element={<PricingPage />} />
       </Routes>
       <Footer />
     </div>
